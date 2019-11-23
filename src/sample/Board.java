3c247323e2da18aa9cdb5df6ac4cad4da9e471f4
Fredric Lundberg <Fredric.idk@outlook.com>
2019-11-22 21:55:00 -0800

    add drawSquare()

--- src/sample/Board.java
+++ src/sample/Board.java
@@ -2,22 +2,41 @@ package sample;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class Board extends JPanel {
 
-    //TODO temporarily added
     public Board() {};
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
 
-        drawSquare(g);
+        drawSquare(graphics);
     }
 
-    public void drawSquare(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+    public void drawSquare(Graphics graphics) {
+        Graphics2D graphics2D = (Graphics2D) graphics;
 
+        RenderingHints renderinghints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        renderinghints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        graphics2D.setRenderingHints(renderinghints);
 
+        Dimension size = getSize();
+        double width = size.getWidth();
+        double height = size.getHeight();
+
+        Rectangle2D rectangle2D = new Rectangle2D.Double(0,0, width, height);
+        initGraphics2D(graphics2D);
+        graphics2D.fill(rectangle2D);
+        graphics2D.draw(rectangle2D);
     }
+
+    private void initGraphics2D(Graphics2D graphics2D) {
+        graphics2D.setStroke(new BasicStroke(1));
+        graphics2D.setColor(new Color(6, 183, 155));
+    }
+
+
+
 
 }
